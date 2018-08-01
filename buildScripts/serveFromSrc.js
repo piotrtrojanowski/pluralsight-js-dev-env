3312eaa9d16a518f088a11f34ad5dc6b6aebaa18
Piotr Trojanowski <Piotr_Trojanowski@mckinsey.com>
2018-08-01 15:34:51 +0200

    js call

--- buildScripts/serveFromSrc.js
+++ buildScripts/serveFromSrc.js
@@ -22,6 +22,14 @@ http_server.get('/', function(req, res) {
 	res.sendFile (path.join(__dirname, '//../src/index.html'));
 });
 
+http_server.get('/users', function(req,res) {
+	res.json([
+		{"id": 1, "firstName":"Bob", "lastName":"Smith","email":"bob@gmail.com"},
+		{"id": 2, "firstName":"Tien", "lastName":"Tiem","email":"tien@gmail.com"},
+		{"id": 3, "firstName":"Sen", "lastName":"Siem","email":"sen@gmail.com"}
+	]);
+});
+
 http_server.listen(port, function(err) {
 	if (err) {
 		console.log(err);
